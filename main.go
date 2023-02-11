200ce9b08ba2fa9e895df21a383ca536c1d6c68d
DFD1 <1240047299@qq.com>
2023-02-11 17:31:57 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type UserInfo struct {
+	ID     uint
+	Name   string
+	Gender string
+	Hobby  string
+}
+type User struct {
+	Model gorm.Model
+	Name  string
+	Age   int
+}
+
+func main() {
+	gin.Default()
+	//连接数据库
+	db, err := gorm.Open("mysql", "root:root@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	db.AutoMigrate(&User{})
+	user := User{Name: "qimi", Age: 18}
+	db.Create(user)
+}
